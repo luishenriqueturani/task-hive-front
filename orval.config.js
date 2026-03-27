1ebbb831dccbef67dba56f968fb60e61babe1407
Luís Henrique <79663087+luishenriqueturani@users.noreply.github.com>
2026-03-27 11:14:55 -0300

    reset do repositório

--- orval.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  taskHiveApi: {
-    output: {
-      target: "src/api/generated.ts",  // Arquivo gerado com os hooks
-      schemas: "src/api/schemas",     // Pasta onde os schemas serão armazenados
-    },
-    input: "./swagger.json",      // Caminho para o JSON do Swagger
-  },
-};
